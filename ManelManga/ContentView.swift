523fcd7c2a116175a8d19e2bce50d7cb0ea50382
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-09-15 10:49:02 -0300

    Função getVolume implementada.

--- ManelManga/ContentView.swift
+++ ManelManga/ContentView.swift
@@ -13,6 +13,28 @@ struct ContentView: View {
     
     @State var images: [String] = []
     
+    func getVolume(url: URL) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data, error == nil {
+                let html = String(data: data, encoding: .utf8)
+                if let html = html {
+                    do {
+                        let doc = try SwiftSoup.parse(html)
+                        let imgs = try doc.select("img[class=wp-manga-chapter-img]")
+                        var imagesList: [String] = []
+                        for img in imgs.array() {
+                            let link = try img.attr("src")
+                            imagesList.append(String(link.suffix(link.count-7)))
+                        }
+                        images = imagesList
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+        }.resume()
+    }
+    
     var body: some View {
         VStack {
             ScrollView {
@@ -33,28 +55,11 @@ struct ContentView: View {
             guard let url = URL(string: link) else {
                 return
             }
-            URLSession.shared.dataTask(with: url) { data, _, error in
-                if let data = data, error == nil {
-                    let html = String(data: data, encoding: .utf8)
-                    if let html = html {
-                        do {
-                            let doc = try SwiftSoup.parse(html)
-                            let imgs = try doc.select("img[class=wp-manga-chapter-img]")
-                            var imagesList: [String] = []
-                            for img in imgs.array() {
-                                let link = try img.attr("src")
-                                imagesList.append(String(link.suffix(link.count-7)))
-                            }
-                            images = imagesList
-                        } catch {
-                            print(error)
-                        }
-                    }
-                }
-            }.resume()
+            getVolume(url: url)
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
