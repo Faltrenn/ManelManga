a63c67d4332917240e30797103ae14845b63f1b5
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-09-15 09:37:55 -0300

    Initial Commit

---
+++ ManelManga/ManelMangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManelMangaApp.swift
+//  ManelManga
+//
+//  Created by Emanuel on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ManelMangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
