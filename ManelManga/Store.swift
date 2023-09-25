0f276528f4fadf846461524b15f9b6b5121a5e91
Emanuel Alves <emanuelalvesp5@gmail.com>
2023-09-25 14:15:59 -0300

    Remoção de código morto.

--- ManelManga/Store.swift
+++ ManelManga/Store.swift
@@ -69,3 +69,72 @@ enum Pages: CaseIterable {
         }
     }
 }
+
+class CustomURLSession: NSObject, ObservableObject, URLSessionDownloadDelegate {
+    @Published var progress: CGFloat = 0.0
+    
+    @Published var downloadTask: URLSessionDownloadTask?
+    
+    private var saveAt: URL?
+    
+    private var completion: ((_ savedAt: URL) -> Void)?
+    
+    func getDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+    
+    func downloadEpisode(anime: Anime, episode: Episode, source: Source, completion: ((_ savedAt: URL) -> Void)? = nil) {
+        self.completion = completion
+        var saveAt = getDirectory().appendingPathComponent(anime.name, isDirectory: true)
+        do {
+            try FileManager.default.createDirectory(at: saveAt, withIntermediateDirectories: true)
+        } catch { }
+        saveAt = saveAt.appendingPathComponent("\(episode.name).\(source.type.split(separator: "/")[1])")
+        self.startDownlod(link: source.file, saveAt: saveAt)
+    }
+    
+    func startDownlod(link: String, saveAt: URL) {
+        guard let url = URL(string: link) else { return }
+        
+        downloadTask = URLSession(configuration: .default,
+                                  delegate: self,
+                                  delegateQueue: .current).downloadTask(with: url)
+        self.saveAt = saveAt
+        DispatchQueue.global(qos: .background).async {
+            self.downloadTask!.resume()
+        }
+    }
+    
+    func cancel() {
+        if let downloadTask = downloadTask {
+            downloadTask.cancel()
+        }
+    }
+    
+    func pause() {
+        if let downloadTask = downloadTask {
+            downloadTask.progress.pause()
+        }
+    }
+    func resume() {
+        if let downloadTask = downloadTask {
+            downloadTask.resume()
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        do {
+            try FileManager.default.moveItem(at: location, to: saveAt!)
+        } catch { }
+        if let completion = completion {
+            completion(saveAt!)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let progress = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+        DispatchQueue.main.async {
+            self.progress = progress
+        }
+    }
+}
